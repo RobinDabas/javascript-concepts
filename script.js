5d71ab927f93382016dfaf49ffcd01b4774fb563
ROBERTO D <robindabas@hotmail.com>
2025-04-23 19:46:24 -0400

    learn variables and functions

---
+++ script.js
@@ -0,0 +1,28 @@
+let Roberto = "Is actively improving his life everyday by doing the right behaviours that will lead him to achieving his goals."
+console.log(Roberto)
+
+function decíQuiubo(nombre) {
+  console.log("¡Quiubo, " + nombre + "!");
+}
+
+decíQuiubo("Roberto")
+
+function sumáNúmeros(a, b) {
+  console.log(a + b);
+}
+
+sumáNúmeros(5, 500);
+sumáNúmeros(500, 15);
+
+function decíMiAnimalFavorito(nombre) {
+  console.log("Mi animal favorito es " + nombre + ".")
+}
+
+decíMiAnimalFavorito("un elephante");
+decíMiAnimalFavorito("un papagayo")
+
+function decíMiAfirmaciónFavorita(afirmación) {
+  console.log("Mi afirmación favorita es: " + afirmación + ".")
+}
+
+decíMiAfirmaciónFavorita("¡Sos inteligente y capaz. Creo en vos!")
